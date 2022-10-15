f0081e483436f6724813f8d5fbcaf8ae0d3a823d
Jin-hyeok Bang <jinhyeokfang@gmail.com>
2022-10-16 01:18:11 +0900

    change website style

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-window.open('http://server.jinhy.uk:22003')
+window.open('http://server.jinhy.uk')
 window.open('https://github.com/jinhyeokfang/?tab=repositories')
 window.open('https://discord.com/channels/@me')
 window.open('https://music.youtube.com')
