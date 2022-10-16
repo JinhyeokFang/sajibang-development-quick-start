43f05f2c1b4bb414eef0b70adc0ba474f37b4d47
Jin-hyeok Bang <jinhyeokfang@gmail.com>
2022-10-16 22:37:10 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-window.open('http://server.jinhy.uk')
+window.open('https://code.jinhy.uk')
+window.open('https://server.jinhy.uk')
 window.open('https://github.com/jinhyeokfang/?tab=repositories')
 window.open('https://discord.com/channels/@me')
 window.open('https://music.youtube.com')
